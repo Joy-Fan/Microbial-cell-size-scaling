c51ec9bef23c1e2a5ee2e1efc7e27886dca2b2bf
Joy-Fan <103389950+Joy-Fan@users.noreply.github.com>
2022-05-15 15:39:21 +0100

    Add files via upload

---
+++ Data Merge.R
@@ -0,0 +1,16 @@
+# Clear the data
+rm(list=ls())
+
+#Import the data
+Madin <- read.csv("C:/Users/FAN/Desktop/Data/Madin.csv", as.is=TRUE)
+Hira <- read.csv("C:/Users/FAN/Desktop/Data/Hira.csv", as.is=TRUE)
+
+#Merge the data
+Total<-rbind(Madin,Hira)
+T <- Total[order(Total$GenusSpecies, -Total$GrowthRate),]
+library(dplyr)
+library(plyr)
+Replicated<-T%>%group_by(GenusSpecies)%>%filter(length(GenusSpecies)>1)
+T<-T[!duplicated(T$GenusSpecies),]
+write.table (T, file ="C:/Users/FAN/Desktop/Data/Merged_Data.csv", sep =",")
+
